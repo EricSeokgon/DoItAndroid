a2a97ea4d93638ff78f96f9ac4f26b0222c08b03
hadeslee <hadesleeq@gmail.com>
2016-11-27 03:32:05 +0900

    버튼 클릭 이벤트 추가

--- app/src/main/java/tk/hadeslee/hello/MainActivity.java
+++ app/src/main/java/tk/hadeslee/hello/MainActivity.java
@@ -1,7 +1,9 @@
 package tk.hadeslee.hello;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "버튼이 클릭되었습니다.",Toast.LENGTH_LONG).show();
     }
 }
